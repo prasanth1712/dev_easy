29f784cdf1fdc1c4bf5bbe829435a283a32961c6
pr1712 <prasanth_r@bmc.com>
2021-02-02 22:45:04 +0530

    Config and PUll request

--- app/bitbucket.py
+++ app/bitbucket.py
@@ -2,17 +2,28 @@ import os
 import subprocess as sp
 from conf import get_conf
 
-cnf = get_conf('BITBUCKET')
+if 'nt' in os.name:
+    cnf = get_conf('BITBUCKET')
+else:
+    cnf = get_conf('BITBUCKET_POSIX')
 
-
-def get_files(search_key,git_path=None):
-
+def get_files(search_key):
     old_dir = os.getcwd()
-    if not git_path:
-        git_path = cnf['local_git_dir']
+
+    git_path = cnf['git_dir']
     os.chdir(git_path)
+    
     if search_key:
-        ot = sp.run(["git","log",'--all','--grep',search_key,'--pretty=format:','--name-only'],capture_output=True)
+        ot = sp.run(["git","diff-index",'--quiet','master'],stdout=sp.PIPE)
+        if int(ot.returncode) != 0:
+            bit_username = cnf['username']
+            bit_password = cnf['password']
+            bit_url = cnf['url']
+            tmp = str(bit_url).split('//')
+            tmp = str(tmp[0])+'//'+str(bit_username)+':'+str(bit_password).replace('@','%40')+'@'+str(tmp[1])
+            ot = sp.run(["git","pull",tmp],stdout=sp.PIPE)
+        
+        ot = sp.run(["git","log",'--all','--grep',search_key,'--pretty=format:','--name-only'],stdout=sp.PIPE)
         lst = str(ot.stdout.decode('UTF-8')).splitlines()
         lst = list(set(lst))
         if '' in lst:
--- conf.py
+++ conf.py
@@ -1,9 +1,9 @@
 import configparser
+import os
 
 def get_conf(sect):
     config = configparser.ConfigParser()
     config.read('config.ini')
     if sect in config:
         return config[sect]
-
 
